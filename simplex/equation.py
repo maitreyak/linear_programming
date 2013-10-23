d0eaeb42e8de2876e90ab52e8fd244b4ac5269b3
Maitreya <maitreyak@gmail.com>
2013-10-23 11:37:32 -0500

    added some methods and tests

--- simplex/equation.py
+++ simplex/equation.py
@@ -1,12 +1,26 @@
 import unittest
 
 class EquationTestClass(unittest.TestCase):
-    
+
+    def setUp(self):
+        self.equation = Equation(1,4.6,{2:-5.7,3:5.7}) 
+
     def test_equation(self):
-        self.assertIsNotNone(Equation([1,2,3,4]))
+        self.assertIsNotNone(self.equation)
+    
+    def test_value_for_entering_var(self):
+        equation = Equation(1,4.0,{2:-2.0,3:2.0})
+        self.assertEquals(equation.value_for_entering_var(2),2.0)
+        self.assertEquals(equation.value_for_entering_var(3),-2.0)
 
 class Equation(object):
     
-    def __init__(self,vector = []):
-        self.vector = vector
+    def __init__(self,basicVar,bValue,rhsDict):
+        self.basicVar = basicVar
+        self.bValue = bValue
+        self.rhsDict = dict(rhsDict)
         
+    def value_for_entering_var(self,enterIndex):
+        coeffValue = self.rhsDict[enterIndex]
+        value = float(self.bValue)/float(-coeffValue)  
+        return value
