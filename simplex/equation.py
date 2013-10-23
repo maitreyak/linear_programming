7379e35ca5f68d8b9d1706ca980cfe76ed4abd10
Maitreya <maitreyak@gmail.com>
2013-10-23 17:27:22 -0500

    added more fuctionality

--- simplex/equation.py
+++ simplex/equation.py
@@ -8,10 +8,35 @@ class EquationTestClass(unittest.TestCase):
     def test_equation(self):
         self.assertIsNotNone(self.equation)
     
-    def test_value_for_entering_var(self):
+    def test_valueOfEntry(self):
         equation = Equation(1,4.0,{2:-2.0,3:2.0})
-        self.assertEquals(equation.value_for_entering_var(2),2.0)
-        self.assertEquals(equation.value_for_entering_var(3),-2.0)
+        self.assertEquals(equation.valueOfEntry(2),2.0)
+        self.assertEquals(equation.valueOfEntry(3),-2.0)
+    
+    def test_valueOfEntryInvalidEntry(self):
+        self.assertIsNone(self.equation.valueOfEntry(9))
+         
+    def test_valueOfEntryZeroCoeff(self):
+        self.equation.rhsDict[10] = 0.0
+        self.assertIsNone(self.equation.valueOfEntry(10))
+     
+    def test_exitVarRebalanceInvalidIndex(self):
+        self.assertFalse(self.equation.exitVarRebalance(103))
+
+    def test_exitVarRebalanceInvalidCoeff(self):
+        self.equation.rhsDict[103] = 0.0
+        self.assertFalse(self.equation.exitVarRebalance(103))
+    
+    def test_exitVarRebalance(self):
+        equation = Equation(1,4.0,{2:-2.0,3:2.0})
+        self.assertTrue(equation.exitVarRebalance(2))
+        self.assertEquals(equation.basicVar,2)
+        self.assertEquals(equation.bValue,2.0)
+        self.assertEquals(equation.rhsDict[1],-0.5)
+        self.assertEquals(equation.rhsDict[3],1.0)
+        self.assertFalse(equation.exitVarRebalance(2))
+
+
 
 class Equation(object):
     
@@ -20,7 +45,34 @@ class Equation(object):
         self.bValue = bValue
         self.rhsDict = dict(rhsDict)
         
-    def value_for_entering_var(self,enterIndex):
+    def valueOfEntry(self,enterIndex):
+        if enterIndex not in self.rhsDict:
+            return None
+        
         coeffValue = self.rhsDict[enterIndex]
+        
+        if coeffValue == float(0):
+            return None
+        
         value = float(self.bValue)/float(-coeffValue)  
         return value
+    
+    def exitVarRebalance(self,varIndex): 
+        if varIndex not in self.rhsDict:
+            return False
+        
+        coeffValue = float(self.rhsDict[varIndex])        
+        
+        if coeffValue == float(0):
+            return False
+        
+        self.rhsDict[self.basicVar] = -1 
+        self.basicVar = varIndex
+        del self.rhsDict[varIndex]
+
+        for key in self.rhsDict.keys():
+            self.rhsDict[key] /= -coeffValue        
+        
+        self.bValue = self.bValue/-coeffValue
+        
+        return True
